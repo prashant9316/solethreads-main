ac0f379e364c8785fd2c56cf04cdbc746e89653c
prashant9316 <48996523+prashant9316@users.noreply.github.com>
2023-03-01 14:02:18 +0530

    fixed navbar bg color in responsive mode

--- nav.js
+++ nav.js
@@ -1,9 +1,23 @@
 const logoSection = document.getElementById('logo-section')
 const linkSection = document.getElementById('nav-links')
-
+let counter = 0;
 $(document).ready(function(){
     $('.btn').click(function(){
         $('.nav-links').slideToggle(2000);
+        console.log(counter);
+        if(counter % 2 == 0){
+            linkSection.style.backgroundColor = '#02405F';
+            logoSection.style.backgroundColor = '#02405F';
+        } 
+        else {
+            console.log("Setting bg color to none")
+            setTimeout(() => {
+                linkSection.style.background = 'none';
+                logoSection.style.background = 'none';
+            }, 2000)
+            
+        }
+        counter+=1;
         //for the button effect
         $('.btn').toggleClass('change');
     })
@@ -15,15 +29,18 @@ $(document).ready(function(){
             if(window.innerWidth >= 992){
                 logoSection.style.display = 'none'
                 logoSection.style.transition = '1s ease';
-                // linkSection.style.background = '#02405F';
-                linkSection.style.transition = '1s ease';
+                // linkSection.style.backgroundColor = 'none';
 
+                linkSection.style.transition = '1s ease';
+                
+                // linkSection.style.background = '#02405F';
             }
         }
         else{
             $('nav,nav-container').removeClass('navBackground')
             if(window.innerWidth >= 992){
                 logoSection.style.display = 'flex';
+                // linkSection.style.backgroundColor = '#02405F';
                 // linkSection.style.background = 'none';
             }
         }
